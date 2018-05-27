b3cbfef4cd727478c3dd1b7fc8f219d4521602e7
M. Saiful Rizal <saifulryzl15@gmail.com>
2018-05-27 21:01:26 +0700

    dicek yaa, masih ada yang error

---
+++ application/views/input/inputTA.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title>Input Tugas Akhir</title>
+	</head>
+	<body>
+		<h4 align="center">Pengajuan Judul Tugas Akhir</h4>
+		<div align="center">
+		<p>
+			<form method="post" action="input">
+        NIM<br/><input type="text" name="nim" size="10" maxlength="10"><br/><br/>
+		
+		
+        Dosen Pembimbing<br/>
+        <select name="nama_dosen">
+        <?php foreach ($nama_dosen as $row){ ?>
+        	<option value="<?php echo $row->id;?>"><?php echo $row->nama_dosen;?></option>
+        	<?php }?>
+        </select>
+        <br/><br/>
+		
+		 Judul Penelitian<br/>
+        <select name="judul_penelitian">
+        <?php foreach ($judul_penelitian as $row){ ?>
+        	<option value="<?php echo $row->id;?>"><?php echo $row->judul_penelitian;?></option>
+        	<?php }?>
+        </select>
+        <br/><br/>
+		
+		Judul<br/><input type="text" name="judul" size="10" maxlength="100"><br/><br/>
+		
+		Deskripsi<br/><input type="text" name="deskripsi" size="10" maxlength="2000"><br/><br/>
+		
+        <input type="submit" name="btnTambah" value="Simpan"/>
+        <a href="<?php echo base_url()?>">Kembali</a>
+    </form>
+		</p>
+		</div>
+	</body>
+</html>
