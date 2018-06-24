f0ce6aa9965d8ff060e8d5ee66833eb1795ddaad
M. Saiful Rizal <saifulryzl15@gmail.com>
2018-06-24 19:43:44 +0700

    controller inpu TA FIX karna besok udah demo demo

--- application/controllers/inputTA.php
+++ application/controllers/inputTA.php
@@ -1,36 +1,45 @@
 <?php
 class inputTA extends CI_Controller{
-	function __construct()
+function __construct()
 	{
 		parent::__construct();
+		$this->load->helper(array('url'));
 		$this->load->model('m_inputTA');
 	}
 	function index(){
 		$this->load->view('dashboard_user');
 		$this->load->view('header');
-		$this->load->view('input/v_inputTA');
-	}
+		$data['nim'] = $this->m_inputTA->get_mhs();
+		$data['nip'] = $this->m_inputTA->get_dosen();
+		$data['id_penelitian'] = $this->m_inputTA->get_penelitian();
+		$this->load->view('input/v_inputTA',$data);
 	}
 	
 	function input(){
 		if (isset($_POST['btnTambah'])){
-			$data = $this->Model_Mahasiswa->input(array (
-			'nim' => $this->input->post('nim'),
-			'nip' => $this->input->post('nama_dosen'),
-			'id_penelitian' => $this->input->post('judul_penelitian'),
+			$data = $this->m_inputTA->input(array (
 			'judul' => $this->input->post('judul'),
-			'deskipsi' => $this->input->post('deskripsi')));
-			redirect('dashboard');
-		}else{
-			$x =$this->Model_Mahasiswa->get_dosen();
+			'tgl_input' => $this->input->post('tgl_input'),
+			'deskripsi' => $this->input->post('deskripsi'),
+			'nim' => $this->input->post('nama'),
+			'nip' => $this->input->post('nama_dosen'),
+			'id_penelitian' => $this->input->post('judul_penelitian')));
+			echo "<script> alert('Anda Berhasil Daftar!!')</script>";
+			redirect('inputTA/index','refresh');
+		}
+		else{
+			$x =$this->m_inputTA->get_mhs();
 			$data = array(
+				'nama'=>$this->m_inputTA->get_mhs(),
 				'nama_dosen'=>$this->m_inputTA->get_dosen(),
 				'judul_penelitian'=>$this->m_inputTA->get_penelitian()
 				);
 			//var_dump($x);
+			echo "<script> alert('Gagal Mendaftar!!')</script>";
 			$this->load->view('input/v_inputTA',$data);
 		}
 	}
+}
 
 ?>
 	
