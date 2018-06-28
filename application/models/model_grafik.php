330f6ad3a32cc750dac76f62c70cfa06228f9f0a
nandaseptian <nandaardika670@gmail.com>
2018-06-28 09:48:00 +0700

    grafik usulan, sebaran dosen, judul fix tapi masih ada yang kurang

--- application/models/model_grafik.php
+++ application/models/model_grafik.php
@@ -1,29 +1,29 @@
+<?php
+class Model_grafik extends CI_Model {
+	function jumlah_siswa(){
+		$query = $this->db->query("SELECT count(distinct nim) as id FROM tb_mahasiswa");
+		return $query->row();
+	}
+	function jumlah_usulan(){
+		$query = $this->db->query("SELECT count(distinct nim) as id FROM tb_judul_usulan");
+		return $query->row();
+	}
+	function belum_input(){
+		$query = $this->db->query("SELECT * FROM tb_mahasiswa WHERE NOT EXISTS (SELECT * FROM tb_judul_usulan WHERE tb_mahasiswa.nim = tb_judul_usulan.nim)");
+		return $query->result();
+	}
+	function t_judul_siswa(){
+		$query = $this->db->query("SELECT count(nim) as id FROM `tb_judul_usulan`");
+		return $query->row();
+	}
+	function t_judul_dosen(){
+		$query = $this->db->query("SELECT count(id_dosen) as id FROM `tb_judul_dosen`");
+		return $query->row();
+	}
+	function sebaran_dosen(){
+		$query = $this->db->query("SELECT tb_dosen.nama ,  count(tb_judul_usulan.id_dosen_pembimbing) as total FROM `tb_judul_usulan` inner join tb_dosen on tb_judul_usulan.id_dosen_pembimbing = tb_dosen.id_dosen group by tb_judul_usulan.id_dosen_pembimbing order by total desc");
+		return $query->result();
+	}
 
-<?php 
-class Model_grafik extends CI_Model{
-   function statistik_pengunjung()
- {
-  
-  $sql= $this->db->query("
-  
-  select
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=1)AND (YEAR(date)=2016))),0) AS `Januari`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=2)AND (YEAR(date)=2016))),0) AS `Februari`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=3)AND (YEAR(date)=2016))),0) AS `Maret`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=4)AND (YEAR(date)=2016))),0) AS `April`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=5)AND (YEAR(date)=2016))),0) AS `Mei`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=6)AND (YEAR(date)=2016))),0) AS `Juni`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=7)AND (YEAR(date)=2016))),0) AS `Juli`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=8)AND (YEAR(date)=2016))),0) AS `Agustus`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=9)AND (YEAR(date)=2016))),0) AS `September`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=10)AND (YEAR(date)=2016))),0) AS `Oktober`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=11)AND (YEAR(date)=2016))),0) AS `November`,
-  ifnull((SELECT count(ip) FROM (ta)WHERE((Month(date)=12)AND (YEAR(date)=2016))),0) AS `Desember`
- from ta GROUP BY YEAR(date) 
-  
-  ");
-  
-  return $sql;
-  
- } 
+}
 ?>
